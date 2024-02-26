4272d79213ac1b041903fb7798658c50d64fb4e9
JulienMnt <julien.menet@edu.devinci.fr>
2024-02-26 16:53:04 +0100

    Essai

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,14 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+"use strict";
+
+function rand(min, max) {
+  max++;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function opencard(){
+  let x = rand(0,30);
+  return fetch("https://hp-api.lainocs.fr/" + x)
+    .then((response) => response.json())
+    
+}
+console.log(opencard());
